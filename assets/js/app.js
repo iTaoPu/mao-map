4a1d20a2b4d92b42552d9f5efc51aacdf0eb2b86
IT咖啡館 <176115520+iTCoffe@users.noreply.github.com>
2025-12-26 11:02:47 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3066,7 +3066,7 @@ const MUSIC_PLAYLIST = [
       // 第二个是维基百科的公共版权音乐
       //"https://raw.githubusercontent.com/iTCoffe/mao-map/master/data/music/Internationale-cmn_(英特纳雄耐尔).ogg",
       "https://mmap.shrimp.hidns.co/data/music/Internationale-cmn_(英特纳雄耐尔).ogg",
-      //"https://upload.wikimedia.org/wikipedia/commons/5/5b/Internationale-cmn_(英特纳雄耐尔).ogg",
+      "https://upload.wikimedia.org/wikipedia/commons/5/5b/Internationale-cmn_(英特纳雄耐尔).ogg",
     ],
   },
   {
@@ -3077,7 +3077,7 @@ const MUSIC_PLAYLIST = [
     urls: [
       //"https://raw.githubusercontent.com/iTCoffe/mao-map/master/data/music/东方红_-_The_East_Is_Red_(1950).ogg",
       "https://mmap.shrimp.hidns.co/data/music/东方红_-_The_East_Is_Red_(1950).ogg",
-      //"https://upload.wikimedia.org/wikipedia/commons/d/d8/东方红_-_The_East_Is_Red_(1950).ogg",
+      "https://upload.wikimedia.org/wikipedia/commons/d/d8/东方红_-_The_East_Is_Red_(1950).ogg",
     ],
   },
 ];
